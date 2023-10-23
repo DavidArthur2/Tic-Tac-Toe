63b28179b3ee8c59b1fabe80bee08b11b256dd0a
arthu <david.arthur@student.ms.sapientia.ro>
2023-10-23 13:08:40 +0300

    Major fixes for stability and performance

--- Gesture Recognition/base_detection.py
+++ Gesture Recognition/base_detection.py
@@ -1,19 +1,25 @@
 import cv2  # Képfeldolgozásra használt könyvtár
 import mediapipe as mp  # Képfelismerésre használt könyvtár -- Tartalmazza a TensorFlowot
 from utils.error import sendError
+from gesture_recognition import calcResult
+import threading
+from memory_profiler import profile
 
 # Default variables
 mp_hands = mp.solutions.hands  # Hasznalt class a kezfelismeresre
 mp_drawing = mp.solutions.drawing_utils  # Hasznalt class a kezrajzolasra
+hands_detector = None # A kepfelismero
 
-cam = cv2.VideoCapture(0)
+cam = None
 
-detection_confidence = 0.7
+detection_confidence = 0.5
 tracking_confidence = 0.5
 
 
+def initialize_camera():
+    global cam
+    cam = cv2.VideoCapture(0)
 
-def CheckCamera():
     if cam is None:
         sendError("Hiba a kameránál", "Nincs inicializálva a kamera!")
         return False
@@ -24,39 +30,50 @@ def CheckCamera():
         return False
     return True
 
-def OperateCamera():
-    if not CheckCamera():
-        return
+def capture_frame():
 
     while cam.isOpened():
 
         res, frame = cam.read()
+
         if not res:
             sendError("Hiba a kamerával", "Megszakadt a kapcsolat a kamerával!")
             return False
 
-        cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        rec_img = mp_hands.Hands(max_num_hands=1, min_detection_confidence=detection_confidence,
-                                 min_tracking_confidence=tracking_confidence).process(frame)
-
-        cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-        if rec_img.multi_hand_landmarks:
-            for hand_landmarks in rec_img.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(frame, hand_landmarks, connections=mp_hands.HAND_CONNECTIONS)
-                x1 = float(str(rec_img.multi_hand_landmarks[-1].landmark[8]).split(' ')[1].split('\n')[0])
-                x2 = float(str(rec_img.multi_hand_landmarks[-1].landmark[12]).split(' ')[1].split('\n')[0])
-                x = abs((x1 - x2))
-                cv2.putText(frame, f'Tavolsag a ket ujj kozott:{x}', (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-
-        cv2.imshow("Kep", frame)
+        process_frame(frame)
 
         if cv2.waitKey(5) & 0xFF == ord("q"):
             break
 
-        cv2.waitKey(1)
+@profile
+def process_frame(frame):
+    cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    rec_img = hands_detector.process(frame)
+
+    if (rec_img.multi_hand_landmarks):
+        hand_landmarks = rec_img.multi_hand_landmarks[0]
+        mp_drawing.draw_landmarks(frame, hand_landmarks, connections=mp_hands.HAND_CONNECTIONS)
+        # calcResult(hand_landmarks)
 
 
 
-OperateCamera()
+    cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    cv2.imshow("Kep", frame)
+
+@profile
+def operate_recognition():
+    global hands_detector
+
+    if not initialize_camera():
+        return
+
+    hands_detector = mp_hands.Hands(max_num_hands=1, min_detection_confidence=detection_confidence,min_tracking_confidence=tracking_confidence)
+    capture_frame()
+
+    hands_detector.close()
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+threading.Thread(target=operate_recognition).start()
---
+++ Gesture Recognition/gesture_recognition.py
@@ -0,0 +1,6 @@
+import mediapipe as mp
+import cv2
+
+
+def calcResult(hand_landmarks):
+    print("Undergoing")
