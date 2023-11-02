df971142814b0f0b159ec9e5826401d55deda871
DavidArthur2 <david.arthur@student.ms.sapientia.ro>
2023-11-02 13:47:23 +0200

    Resolved conflicts after merging with UI

---
+++ main.py
@@ -0,0 +1,17 @@
+import threading
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Gesture Recognition'))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'utils'))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'UI'))
+from base_recognition import operate_recognition
+
+
+def print_hi(name):
+    print(f'Ez a master branch {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Szar3')
+    # threading.Thread(target=operate_recognition).start()
+
